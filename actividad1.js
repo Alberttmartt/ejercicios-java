1d2896fd072689c68a6debd942f983b2a6bd7875
Alberto Martinez <alberto.martinez08@gmail.com>
2021-06-03 17:38:39 -0500

    hola

---
+++ actividad1.js
@@ -0,0 +1,14 @@
+
+var nombre = prompt('¿cual es tu nombre?');
+
+var numero1 = parseInt(prompt("Escribe un numero"));
+var numero2 = parseInt(prompt("Escribe otro numero"));
+var suma = (numero1 + numero2);
+var resta = (numero1 - numero2);
+var multiplicacion =(numero1 * numero2);
+var divicion = (numero1 / numero2);
+
+console.log ("la suma es " + suma)
+console.log ("la resta es" + resta)
+console.log ("la multiplicacion es "+multiplicacion)
+console.log("la division es "+ divicion)
