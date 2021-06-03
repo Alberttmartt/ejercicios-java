d3e36ca83f2a439370f5ea69f1237eab44a5b3ca
Alberto Martinez <alberto.martinez08@gmail.com>
2021-06-03 17:43:32 -0500

    hola

---
+++ actividad4.js
@@ -0,0 +1,27 @@
+alert("Estos son los meses del año");
+var meses=[
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Semptiembre",
+    "octubre",
+    "Noviembre",
+    "Diciembre",
+];
+
+console.log(meses[00]);
+console.log(meses[01]);
+console.log(meses[02]);
+console.log(meses[04]);
+console.log(meses[05]);
+console.log(meses[06]);
+console.log(meses[07]);
+console.log(meses[08]);
+console.log(meses[09]);
+console.log(meses[10]);
+console.log(meses[11]);
