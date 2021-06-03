ffa74971b8a7d890d84ae57531974650a244fc8f
Alberto Martinez <alberto.martinez08@gmail.com>
2021-06-03 17:40:01 -0500

    hola

---
+++ actividad2.js
@@ -0,0 +1,10 @@
+
+
+    var edad = prompt('¿Ingresa tu edad?');
+
+if (edad>=18 && edad<=29){ 
+    alert ("Felicidades miembro de Generation 2021.");}
+ else if (edad<18){
+    alert ("Espere a cumplir los requisitos.");}
+if (edad>=30){
+    alert ("Espere a cumplir con los requisitos.");}
